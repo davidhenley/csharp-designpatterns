38818a269eacb93cfe9500863ff9b074c0aded9f
davidhenley <henleydr@gmail.com>
2019-11-13 10:06:17 -0600

    PubSub using delegates

---
+++ PubSub/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PubSub
+{
+    class Pub
+    {
+        public Action OnChange { get; set; }
+
+        public void Raise()
+        {
+            if (OnChange != null)
+            {
+                OnChange();
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a publisher
+            var p = new Pub();
+
+            // Subscribe to the publisher's events
+            p.OnChange += () => Console.WriteLine("Subscriber 1!");
+            p.OnChange += () => Console.WriteLine("Subscriber 2!");
+
+            p.Raise();
+
+            Console.WriteLine("Press enter to terminate!");
+
+            Console.ReadLine();
+        }
+    }
+}
